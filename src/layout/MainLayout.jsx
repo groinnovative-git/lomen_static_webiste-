eed97d7229129671e7236889f7658a196ec3062a
Avin <avin@vaanam.com>
2026-08-19 22:36:34 +0530

    headerbanner

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import Navbar from '../includes/Navbar'
-import CTA from '../includes/CTA'
 
 const MainLayout = ({children}) => {
   return (
     <div className='mainModule'>
-      <CTA />
       <Navbar />
       <div className='layout'>
         {children}
